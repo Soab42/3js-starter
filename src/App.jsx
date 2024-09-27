92ab25804dd20f56b660627a46d9abd83213ee47
soab42 <syfuddhin@gmail.com>
2024-09-27 19:08:33 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './sections/Hero.jsx';
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
